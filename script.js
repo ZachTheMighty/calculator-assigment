ffb1e6e31f13bd10d2422616d28cc76a51942798
ZachTheMighty <promoted66@gmail.com>
2026-04-06 19:16:27 +0000

    Changed colors of calculator

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let numbersContainer = document.querySelector(".numbers");
+
 function createNumbers() {
   for (let i = 9; i >= 1; i--) {
     let button = document.createElement("button");
@@ -9,3 +10,12 @@ function createNumbers() {
 }
 
 createNumbers();
+
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("mouseenter", () => button.classList.toggle("hover"));
+  button.addEventListener("mouseleave", () => button.classList.toggle("hover"));
+});
+
+buttons.forEach((button) => console.log(button.textContent));
