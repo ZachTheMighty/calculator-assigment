4514c5ae04b206c9f0013842328fa71c40875596
ZachTheMighty <promoted66@gmail.com>
2026-04-07 15:12:29 +0000

    Added functions for each basic math operator

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ function createNumbers() {
     numbersContainer.appendChild(button);
   }
 }
+
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return Number((a / b).toFixed(2));
+}
