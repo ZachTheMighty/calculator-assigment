8340642bd5ef02a6e5420c988578128db93b3006
ZachTheMighty <promoted66@gmail.com>
2026-04-07 15:11:50 +0000

    Changed display background color

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 let numbersContainer = document.querySelector(".numbers");
 
-function createNumbers() {
-  for (let i = 9; i >= 1; i--) {
-    let button = document.createElement("button");
-    button.textContent = `${i}`;
-    button.classList.add("number");
-    numbersContainer.appendChild(button);
-  }
-}
-
 createNumbers();
 
 let buttons = document.querySelectorAll("button");
@@ -18,4 +9,11 @@ buttons.forEach((button) => {
   button.addEventListener("mouseleave", () => button.classList.toggle("hover"));
 });
 
-buttons.forEach((button) => console.log(button.textContent));
+function createNumbers() {
+  for (let i = 9; i >= 1; i--) {
+    let button = document.createElement("button");
+    button.textContent = `${i}`;
+    button.classList.add("number");
+    numbersContainer.appendChild(button);
+  }
+}
