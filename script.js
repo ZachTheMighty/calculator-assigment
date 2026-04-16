1b027ff1552fae10a7a3626237e295d0262dbe69
ZachTheMighty <promoted66@gmail.com>
2026-04-16 19:00:03 +0000

    Added keyboard support

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let numbersContainer = document.querySelector(".numbers");
 createNumbers();
 
 let operators = document.querySelectorAll(".operator");
+let asterisk = document.querySelector(".asterisk");
 let operatorPressed = false;
 let numbers = document.querySelectorAll(".number");
 let display = document.querySelector(".display");
@@ -19,11 +20,6 @@ let a = null,
   operatorsPressed = [],
   i = 0;
 
-buttons.forEach((button) => {
-  button.addEventListener("mouseenter", () => button.classList.toggle("hover"));
-  button.addEventListener("mouseleave", () => button.classList.toggle("hover"));
-});
-
 function createNumbers() {
   for (let i = 9; i >= 1; i--) {
     let button = document.createElement("button");
@@ -34,20 +30,20 @@ function createNumbers() {
 }
 
 function add(a, b) {
-  return Number((a + b).toFixed(2));
+  return Number((a + b).toFixed(3));
 }
 
 function substract(a, b) {
-  return Number((a - b).toFixed(2));
+  return Number((a - b).toFixed(3));
 }
 
 function multiply(a, b) {
-  return Number((a * b).toFixed(2));
+  return Number((a * b).toFixed(3));
 }
 
 function divide(a, b) {
   if (b === 0) return "Don't divide by 0 plz";
-  return Number((a / b).toFixed(2));
+  return Number((a / b).toFixed(3));
 }
 
 function operate(a, b, operator) {
@@ -64,52 +60,57 @@ function operate(a, b, operator) {
   return "Invalid operator";
 }
 
-function updateOperator() {
+function updateOperators(op) {
+  if (a === null) return;
+
+  operatorsPressed.push(op.textContent);
+  numberOfTerms++;
+
+  decimalPoint.disabled = false;
+  decimalPoint.classList.remove("disabled");
+
+  if (numberOfTerms > 1 && a !== null && b !== null && b !== 0) {
+    display.textContent = operate(a, b, operatorsPressed[i]);
+    a = operate(a, b, operatorsPressed[i++]);
+    totalDisplayForB = "";
+    totalDigitForB = "";
+    b = null;
+  }
+}
+
+function addClickEventsToOperators() {
   operators.forEach((op) =>
-    op.addEventListener("click", () => {
-      if (a === null) return;
-
-      operatorsPressed.push(op.textContent);
-      numberOfTerms++;
-
-      decimalPoint.disabled = false;
-      decimalPoint.classList.remove("disabled");
-
-      if (numberOfTerms > 1 && a !== null && b !== null && b !== 0) {
-        display.textContent = operate(a, b, operatorsPressed[i]);
-        a = operate(a, b, operatorsPressed[i++]);
-        totalDisplayForB = "";
-        totalDigitForB = "";
-        b = null;
-      }
-    }),
+    op.addEventListener("click", () => updateOperators(op)),
   );
 }
 
-function updateLiterals() {
+function updateLiterals(number) {
+  if (number.textContent === "." && decimalPoint.disabled === true) return;
+  if (display.textContent === "0") display.textContent = "";
+
+  if (number.textContent === ".") {
+    decimalPoint.disabled = true;
+    decimalPoint.classList.add("disabled");
+  }
+
+  if (operatorsPressed.length === 0) {
+    totalDigitForA += number.textContent;
+    totalDisplayForA += number.textContent;
+    display.textContent = totalDisplayForA;
+  }
+
+  if (operatorsPressed.length !== 0) {
+    totalDigitForB += number.textContent;
+    totalDisplayForB += number.textContent;
+    display.textContent = totalDisplayForB;
+  }
+  if (numberOfTerms === 0) a = +totalDigitForA;
+  b = +totalDigitForB;
+}
+
+function addClickEventsToNumbers() {
   numbers.forEach((number) =>
-    number.addEventListener("click", () => {
-      if (display.textContent === "0") display.textContent = "";
-
-      if (number.textContent === ".") {
-        decimalPoint.disabled = true;
-        decimalPoint.classList.add("disabled");
-      }
-
-      if (operatorsPressed.length === 0) {
-        totalDigitForA += number.textContent;
-        totalDisplayForA += number.textContent;
-        display.textContent = totalDisplayForA;
-      }
-
-      if (operatorsPressed.length !== 0) {
-        totalDigitForB += number.textContent;
-        totalDisplayForB += number.textContent;
-        display.textContent = totalDisplayForB;
-      }
-      if (numberOfTerms === 0) a = +totalDigitForA;
-      b = +totalDigitForB;
-    }),
+    number.addEventListener("click", () => updateLiterals(number)),
   );
 }
 
@@ -168,21 +169,82 @@ function invertColors() {
   });
 }
 
-function displayResult() {
-  updateOperator();
-  updateLiterals();
-  allClear();
-  invertColors();
+function keySupport() {
+  document.addEventListener("keydown", (event) => {
+    buttons.forEach((button) => {
+      if (event.key === button.textContent) {
+        if (button.classList.contains("number")) {
+          updateLiterals(button);
+          if (event.key === ".") return;
+          button.classList.toggle("invert-number");
+        }
 
-  equal.addEventListener("click", () => {
-    if (operatorsPressed.length === 0 || b === null) return;
-    display.textContent = operate(
-      a,
-      b,
-      operatorsPressed[operatorsPressed.length - 1],
-    );
-    clearCalculator();
+        if (button.classList.contains("operator")) {
+          updateOperators(button);
+          button.classList.toggle("invert-operator");
+        }
+      }
+    });
+
+    if (event.key === "*") {
+      updateOperators(asterisk);
+      asterisk.classList.toggle("invert-operator");
+    }
+
+    if (event.key === "=" || event.key === "Enter") {
+      displayResult();
+      equal.classList.toggle("invert-equal");
+    }
+
+    if (event.key === "Delete") {
+      clearCalculator();
+      clear.classList.toggle("invert-clear");
+      display.textContent = "0";
+    }
+  });
+
+  document.addEventListener("keyup", (event) => {
+    buttons.forEach((button) => {
+      if (event.key === button.textContent) {
+        if (button.classList.contains("number")) {
+          if (event.key === ".") return;
+          button.classList.toggle("invert-number");
+        }
+        if (button.classList.contains("operator"))
+          button.classList.toggle("invert-operator");
+      }
+    });
+
+    if (event.key === "*") asterisk.classList.toggle("invert-operator");
+
+    if (event.key === "=" || event.key === "Enter")
+      equal.classList.toggle("invert-equal");
+
+    if (event.key === "Delete") {
+      clear.classList.toggle("invert-clear");
+      display.textContent = "0";
+    }
   });
 }
 
-displayResult();
+function displayResult() {
+  if (operatorsPressed.length === 0 || b === null) return;
+  display.textContent = operate(
+    a,
+    b,
+    operatorsPressed[operatorsPressed.length - 1],
+  );
+  clearCalculator();
+}
+
+function runCalculator() {
+  addClickEventsToOperators();
+  addClickEventsToNumbers();
+  allClear();
+  invertColors();
+  keySupport();
+
+  equal.addEventListener("click", () => displayResult());
+}
+
+runCalculator();
